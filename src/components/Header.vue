42490d8627cb200201ae0afcaf8a6db00a392dd0
tomiseobi <namntse170246@fpt.edu.vn>
2025-07-19 03:05:10 +0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,7 +19,6 @@
           <router-link to="/surveys" exact-active-class="active">Khảo sát</router-link>
           <router-link to="/consultants" exact-active-class="active">Tư vấn</router-link>
           <router-link to="/my-participations" exact-active-class="active">Chương trình của tôi</router-link>
-          <router-link to="/appointments" exact-active-class="active">Cuộc hẹn</router-link>
         </template>
 
         <!-- Admin Links -->
@@ -29,10 +28,6 @@
           <router-link to="/community-programs" exact-active-class="active">Quản lý chương trình</router-link>
           <router-link to="/admin/surveys" exact-active-class="active">Quản lý khảo sát</router-link>
           <router-link to="/admin/consultants" exact-active-class="active">Quản lý chuyên viên</router-link>
-        </template>
-        <!-- Consultant Links -->
-        <template v-if="isLoggedIn && userRole === 'consultant'">
-          <router-link to="/appointment" exact-active-class="active">Cuộc hẹn</router-link>
         </template>
         <!-- User Info & Logout -->
         <div v-if="isLoggedIn" class="user-info">
